0da0266c209a8273df1b6f1d2c49bce3414ecd71
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-07-13 17:17:56 +0400

    implement the substructure search function
    Homework 4

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
-def substructure_search(mols, mol):
-    pass
+from typing import List
+from rdkit import Chem
+# from rdkit.Chem import Draw
+
+def substructure_search(mols: List[str], mol: str) -> List[str]:
+    """ 
+    Function returns a list of all molecules as SMILES strings from mols 
+    that contain substructure mol as SMILES string.
+    """
+    mol = Chem.MolFromSmiles(mol)
+    return [smiles for smiles in mols if Chem.MolFromSmiles(smiles).HasSubstructMatch(mol)]
