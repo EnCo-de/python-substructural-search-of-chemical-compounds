9dbc8ce7ff7aff8c23e5e32a247d2c41e6061a94
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-07-17 19:37:16 +0400

    implement the substructure search function
    with example

--- src/main.py
+++ src/main.py
@@ -8,4 +8,10 @@ def substructure_search(mols: List[str], mol: str) -> List[str]:
     that contain substructure mol as SMILES string.
     """
     mol = Chem.MolFromSmiles(mol)
-    return [smiles for smiles in mols if Chem.MolFromSmiles(smiles).HasSubstructMatch(mol)]
+    return [smiles for smiles in mols 
+            if Chem.MolFromSmiles(smiles).HasSubstructMatch(mol)]
+
+if __name__ == "__main__":
+    example = substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1")
+    # ["c1ccccc1", "CC(=O)Oc1ccccc1C(=O)O"]
+    print(example)
