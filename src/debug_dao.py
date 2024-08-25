333ff7526a5b13313c6fbc99419a7aff94796e7f
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-08-25 14:54:50 +0400

    begin with SQLAlchemy 3

--- src/debug_dao.py
+++ src/debug_dao.py
@@ -1,12 +1,23 @@
 from dao import MoleculeDAO
 
-s = MoleculeDAO.get(id=2)
-print(type(s), s)
+# z = MoleculeDAO.create(smiles='GUESS')
+# print(z)
+# z = MoleculeDAO.create(id=7, smiles='Buzz')
+# print(MoleculeDAO.delete(7))
+# s = MoleculeDAO.get(id=2)
+# print(type(s), s)
 s = MoleculeDAO.all()
 print(type(s), s)
-for x in s:
-    print(type(x), x)
+# for x in s:
+#     print(type(x), x)
+MoleculeDAO.insert('Hugo Boss')
+MoleculeDAO.insert('Louis', 'Viton', 'Chu')
+
 print(MoleculeDAO.filter(smiles='y'))
-print(MoleculeDAO.smiles())
+print(MoleculeDAO.get(id=3, smiles='y'))
+MoleculeDAO.update(id=20, smiles='Hermes')
+# print()
 print(MoleculeDAO.get(smiles='Yeah'))
+print(MoleculeDAO.all())
+print(MoleculeDAO.smiles())
 # s = MoleculeDAO.get(id=22)
