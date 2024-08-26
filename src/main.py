4f97472640d34d709520af81f519fcb9c101a604
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-08-26 20:41:01 +0400

    Update src/main.py with dao.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,10 @@ from typing import List  # , Union, Optional
 from rdkit.Chem import MolFromSmiles  # , Draw
 from fastapi import FastAPI, status, HTTPException, UploadFile
 from pydantic import BaseModel
+from sqlalchemy.exc import IntegrityError, NoResultFound, SQLAlchemyError
 from os import getenv
+from src.dao import MoleculeDAO
+# from .dao import MoleculeDAO
 
 
 def substructure_search(mols: List[str], mol: str) -> List[str]:
@@ -47,75 +50,84 @@ def retrieve_all_molecules(limit: int = 100, offset: int = 0):
     Beginning from *offset, limit* the number of molecules in the response.
     '''
     # TODO: remake the function into an iterator
-    return list(molecules.values())
+    return MoleculeDAO.all(limit, offset)
 
 
 @app.post("/smiles/", status_code=status.HTTP_201_CREATED,
           tags=['Storing molecule SMILES'])
-def add_molecule_smiles(new: Molecule):
-    if new.identifier in molecules:
+def add_molecule_smiles(smiles: str):
+    if MolFromSmiles(smiles) is None:
         raise HTTPException(
             status_code=400,
-            detail=f"The molecule with identifier {new.identifier} \
-                is found, duplicate key raised an IntegrityError"
+            detail=("SMILES Parse Error: syntax error "
+                f"for input '{smiles}'.")
+                )
+    try:
+        MoleculeDAO.create(smiles=smiles)
+    except IntegrityError as e:
+        print(e)
+        raise HTTPException(
+            status_code=400,
+            detail=("The molecule is found, duplicate raised "
+                "an IntegrityError: UNIQUE constraint failed")
                 )
     else:
-        if MolFromSmiles(new.smiles) is None:
-            raise HTTPException(
-                status_code=400,
-                detail=f"SMILES Parse Error: syntax error \
-                    for input: {new.smiles}"
-                    )
-        molecules[new.identifier] = new
-        return molecules[new.identifier]
+        return MoleculeDAO.last(smiles=smiles)
 
 
 @app.get("/smiles/{identifier}", tags=['Checking stored molecule SMILES'])
 def retrieve_molecule_by_id(identifier: int):
-    if identifier in molecules:
-        return molecules[identifier]
-    raise HTTPException(
-        status_code=404,
-        detail=f"The molecule with identifier {identifier} is not found."
-        )
+    try:
+        instance = MoleculeDAO.get(id=identifier)
+    except NoResultFound as e:
+        print(e)
+        raise HTTPException(
+            status_code=404,
+            detail=f"The molecule with identifier {identifier} is not found."
+            )
+    else:
+        return instance
 
 
 @app.put("/smiles/{identifier}", tags=['Storing molecule SMILES'])
-def update_molecule(identifier: int, updated: Molecule):
+def create_update_molecule(identifier: int, updated: Molecule):
     if identifier != updated.identifier:
         raise HTTPException(
             status_code=400,
-            detail=f"The molecule identifiers {identifier} \
-                != {updated.identifier} do not match."
+            detail=("The molecule identifiers do not match. "
+                    f"{identifier} != {updated.identifier}")
                 )
-    elif identifier in molecules:
-        if MolFromSmiles(updated.smiles) is None:
-            raise HTTPException(
-                status_code=400,
-                detail=f"SMILES Parse Error: syntax error \
-                    for input: {updated.smiles}"
-                    )
-        molecules[identifier] = updated
-        return molecules[identifier]
-    else:
+    if MolFromSmiles(updated.smiles) is None:
         raise HTTPException(
-            status_code=404,
-            detail=f"A molecule with identifier {identifier} is not found."
-            )
+            status_code=400,
+            detail=f"SMILES Parse Error: syntax error \
+                for input: {updated.smiles}"
+                )
+    try:
+        MoleculeDAO.update(id=identifier, smiles=updated.smiles)
+    except NoResultFound as e:
+        print(e)
+        MoleculeDAO.create(id=identifier, smiles=updated.smiles)
+        # TODO: 201 Created
+    finally:
+        return MoleculeDAO.get(id=identifier)
 
 
 @app.delete("/smiles/{identifier}", tags=['Storing molecule SMILES'],
             response_description="The molecule is deleted",
             summary="Delete a molecule from storage by identifier")
 def delete_molecule(identifier: int):
-    if identifier in molecules:
-        return molecules.pop(identifier)
-    else:
+    try:
+        instance = MoleculeDAO.delete(id=identifier)
+    except NoResultFound as e:
+        print(e)
         raise HTTPException(
             status_code=404,
-            detail=f"The molecule with identifier \
-                {identifier} is not found."
+            detail=("The molecule with identifier "
+                    f"{identifier} is not found.")
             )
+    else:
+        return instance
 
 
 @app.get("/search/{mol}", tags=['Substructure search'])
@@ -127,9 +139,9 @@ def search_molecules(mol: str = None):
     - **return** a list of all molecules that contain
     substructure `mol` as SMILES strings
     """
+    molecules = MoleculeDAO.smiles()
     if len(molecules) > 0 and mol is not None:
-        mols = [molecule.smiles for molecule in molecules.values()]
-        return substructure_search(mols, mol)
+        return substructure_search(molecules, mol)
     else:
         raise HTTPException(
             status_code=400,
@@ -140,14 +152,15 @@ def search_molecules(mol: str = None):
 @app.post("/molecules/", status_code=status.HTTP_201_CREATED,
           summary="[Optional] Upload file with molecules",
           tags=['Substructure search'])
-async def upload_molecules(file: UploadFile | None = None,
-                           n: int = float('inf'), start: int = 1):
+async def upload_molecules(file: UploadFile | None = None):
+                        #   , n: int = float('inf'), start: int = 1):
     """
-    *[Optional]* Upload `n` molecules and add smiles to container
-    starting from identifier `start`
+    *[Optional]*
     ---
     Upload a text `file` with molecules as SMILES strings on separate lines.
     """
+    # Upload `n` molecules and add smiles to container
+    # starting from identifier `start`
     if file is None:
         upload = ["CCO", "c1ccccc1", "Cc1ccccc1", "C(=O)O",
                   "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"]
@@ -160,21 +173,16 @@ async def upload_molecules(file: UploadFile | None = None,
             status_code=400,
             detail="Upload a text file with molecules as SMILES strings."
             )
-    i = start
-    end = min(len(upload), max(0, n)) + start
-    while i < end:
-        smiles = upload[i - start].strip()
-        if smiles and MolFromSmiles(smiles) is not None:
-            molecules[i] = Molecule(identifier=i, smiles=smiles)
-        i += 1
-    return list(molecules.values())
+    smiles = []
+    for s in upload:
+        s = s.strip()
+        if s and s not in smiles and MolFromSmiles(s) is not None:
+            smiles.append(s)
+    MoleculeDAO.insert(*smiles)
+    return MoleculeDAO.last(limit=len(smiles))
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    example = substructure_search(
-        ["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"],
-        "c1ccccc1")
-    assert example == ["c1ccccc1", "CC(=O)Oc1ccccc1C(=O)O"]
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
