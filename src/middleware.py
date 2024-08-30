8818675d2ea0759041e93ab4191932b85513cb35
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-08-30 22:15:22 +0400

    Create logging middleware

---
+++ src/middleware.py
@@ -0,0 +1,19 @@
+from starlette.requests import Request
+import time
+from src.logger import logger
+
+
+# @app.middleware("http")
+async def log_process_time(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    log_extra =  {
+        'method': request.method,
+        'url': request.url.path,
+        'query': request.get(),
+        'body': request.form(),
+        'process_time': round(process_time, 2),
+    }
+    logger.info(log_extra, extra=log_extra)
+    return response
