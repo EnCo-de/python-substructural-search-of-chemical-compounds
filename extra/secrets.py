2e1f82d3b65ea3aa00602e2155af343be0caf63e
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-09-21 19:37:21 +0400

    Update files

--- extra/secrets.py
+++ extra/secrets.py
@@ -1,5 +1,5 @@
 import os
 
-print(os.getenv('ACCESS_KEY')[:7])
-print(os.getenv('SECRET_ACCESS_KEY')[:4:-1])
-print(os.getenv('SECRET_ACCESS_KEY')[-5:-1])
+print(os.getenv('ACCESS_KEY', 'SECRET_ACCESS_KEY')[:7])
+print(os.getenv('SECRET_ACCESS_KEY', 'SECRET_ACCESS_KEY')[:4:-1])
+print(os.getenv('SECRET_ACCESS_KEY', 'SECRET_ACCESS_KEY')[-5:-1])
